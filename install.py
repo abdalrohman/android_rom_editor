f49faadda60a388fee26d8fa2c8993953895e438
abdalrohman <abdd199719@gmail.com>
2022-04-08 20:19:42 +0200

    fix install boot,dtbo,vbmeta,vbmeta_system img

--- install.py
+++ install.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import os
-import re
+from pathlib import Path
 
 from config import load_config
 from create_ext4 import build_dir
@@ -14,36 +14,30 @@ source_dir = os.path.join(load_config('MAIN', 'main_project'), "source")
 if not os.path.isfile(fastboot):
   exit(1)
 
+for boot in Path(source_dir).rglob('boot.img'):
+  RunCommand([fastboot, 'flash', 'boot', str(boot)], verbose=True)
+
+for dtbo in Path(source_dir).rglob('dtbo.img'):
+  RunCommand([fastboot, 'flash', 'dtbo', str(dtbo)], verbose=True)
+
+for vbmeta in Path(source_dir).rglob('vbmeta.img'):
+  RunCommand([fastboot, 'flash', 'vbmeta', str(vbmeta)], verbose=True)
+
+for vbmeta_system in Path(source_dir).rglob('vbmeta_system.img'):
+  RunCommand([fastboot, 'flash', 'vbmeta_system',
+              str(vbmeta_system)], verbose=True)
+
+for img in os.listdir(build_dir):
+  RunCommand([fastboot, 'flash', img.split('.sparse')[0],
+              os.path.join(build_dir, img)], verbose=True)
+
+
 ans = input(
   "It will delete all your files and photos stored on internal storage. [y/n]: ")
-
-
 if ans.lower() == 'y':
-  for img in os.listdir(source_dir):
-    if re.search(rf"('boot.img')", img) is not None:
-      RunCommand([fastboot, 'flash', 'boot', os.path.join(
-        source_dir, img)], verbose=True)
-
-    if re.search(rf"('dtbo.img')", img) is not None:
-      RunCommand([fastboot, 'flash', 'dtbo', os.path.join(
-        source_dir, img)], verbose=True)
-
-    if re.search(rf"('vbmeta.img')", img) is not None:
-      RunCommand([fastboot, 'flash', 'vbmeta', os.path.join(
-        source_dir, img)], verbose=True)
-
-    if re.search(rf"('vbmeta_system.img')", img) is not None:
-      RunCommand([fastboot, 'flash', 'vbmeta_system', os.path.join(
-        source_dir, img)], verbose=True)
-
-  for img in os.listdir(build_dir):
-    RunCommand([fastboot, 'flash', img.split('.sparse')[0],
-               os.path.join(build_dir, img)], verbose=True)
-
   RunCommand([fastboot, 'erase', 'metadata'], verbose=True)
   RunCommand([fastboot, 'erase', 'userdata'], verbose=True)
+
+ans = input("If you want to reboot [y/n]")
+if ans.lower() == 'y':
   RunCommand([fastboot, 'reboot'], verbose=True)
-
-
-else:
-  exit(1)
